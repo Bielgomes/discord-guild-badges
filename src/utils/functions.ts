ae9a6b598f09712e0f3c973b3f1c464f943f33cf
Bielgomes <bielgomesdasilva@hotmail.com>
2025-08-25 01:57:44 -0300

    feat(utils): add fetch image and transform to base64 function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -10,3 +10,16 @@ export function getOnlineMembersCount(guild: Guild) {
     guildOnlineMemberCount,
   }
 }
+
+export async function fetchImageAndTransformToBase64(url: string) {
+  const response = await fetch(url)
+
+  if (response.status !== 200) {
+    throw new Error('Cannot get image')
+  }
+
+  const arrayBuffer = await response.arrayBuffer()
+  const imageBase64 = `data:image/svg+xml;base64,${Buffer.from(arrayBuffer).toString('base64')}`
+
+  return imageBase64
+}
