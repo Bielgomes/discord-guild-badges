bbf1fddbce8e9a8af6fb0bceb95004413e164055
Bielgomes <bielgomesdasilva@hotmail.com>
2025-08-25 18:40:04 -0300

    style(utils): adjust fonts, colors and spacing in make badges

--- src/utils/badges.ts
+++ src/utils/badges.ts
@@ -3,7 +3,7 @@ import { numberFormatter } from './formatters.ts'
 const fontImport = `
   <defs>
     <style>
-      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&amp;display=swap');
+      @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;600;700&amp;display=swap');
       text {
         font-family: roboto, -apple-system, system-ui, "Segoe UI", sans-serif;
       }
@@ -17,7 +17,7 @@ interface makeDefaultBadgeInput {
   guildName: string
   totalOnlineMembersCount: number
   totalMembersCount: number
-  buttonMessage?: string | undefined
+  buttonMessage: string | undefined
 }
 
 export async function makeDefaultBadge({
@@ -49,21 +49,21 @@ export async function makeDefaultBadge({
   const totalMembersStartX = defaultPadding + totalOnlineMembersWidth + 30
 
   const badge = `
-    <svg width="342" height="189" fill="#393A41" xmlns="http://www.w3.org/2000/svg">
+    <svg width="342" height="189" fill="#141414" xmlns="http://www.w3.org/2000/svg">
       ${fontImport}
       <defs>
         <clipPath id="bannerClip">
-          <rect width="100%" height="60" x="0" y="0" rx="4" ry="4" />
+          <rect width="100%" height="60" x="0" y="0" rx="2" ry="2" />
         </clipPath>
         <clipPath id="iconClip">
           <rect width="50" height="50" x="18" y="35" rx="10" ry="10" />
         </clipPath>
       </defs>
-      <rect width="100%" height="100%" rx="4" ry="4" />
+      <rect width="100%" height="100%" rx="2" ry="2" />
 
       <image width="100%" height="60" x="0" y="0" clip-path="url(#bannerClip)" preserveAspectRatio="xMidYMid slice" href="${bannerUrl}" />
 
-      <rect width="54" height="54" x="16" y="33" rx="11" ry="11" fill="#393A41" />
+      <rect width="54" height="54" x="16" y="33" rx="11" ry="11" fill="#141414" />
       <image width="50" height="50" x="18" y="35" clip-path="url(#iconClip)" href="${iconUrl}" />
 
       <text x="15" y="106" font-weight="bold" font-size="20" fill="#FFFFFF" letter-spacing="-0.5">${slicedGuildName}</text>
@@ -75,8 +75,8 @@ export async function makeDefaultBadge({
         <text x="${totalMembersStartX}" y="126" letter-spacing="-0.5">${formattedTotalMembersCount} members</text>
       </g>
 
-      <rect width="312" height="25" x="15" y="145" rx="6" ry="6" fill="#00863A" />
-      <text width="312" height="25" x="171" y="161" text-anchor="middle" font-weight="500" font-size="12" letter-spacing="-0.5" word-spacing="1.2" fill="#FFFFFF">${slicedButtonMessage}</text>
+      <rect width="312" height="30" x="15" y="145" rx="6" ry="6" fill="#00863A" />
+      <text width="312" height="30" x="171" y="160" text-anchor="middle" dominant-baseline="middle" font-weight="600" font-size="14" letter-spacing="-0.5" word-spacing="1.2" fill="#FFFFFF">${slicedButtonMessage}</text>
     </svg>
   `.trim()
 
@@ -90,7 +90,7 @@ interface makeCompactBadgeInput {
   guildName: string
   totalOnlineMembersCount: number
   totalMembersCount: number
-  buttonMessage?: string | undefined
+  buttonMessage: string | undefined
 }
 
 export async function makeCompactBadge({
@@ -121,14 +121,14 @@ export async function makeCompactBadge({
   const totalMembersStartX = defaultPadding + totalOnlineMembersWidth + 90
 
   const badge = `
-    <svg width="390" height="119" fill="#393A41" xmlns="http://www.w3.org/2000/svg">
+    <svg width="388" height="119" fill="#141414" xmlns="http://www.w3.org/2000/svg">
       ${fontImport}
       <defs>
         <clipPath id="iconClip">
           <rect width="50" height="50" x="15" y="15" rx="10" ry="10" />
         </clipPath>
       </defs>
-      <rect width="100%" height="100%" rx="4" ry="4" />
+      <rect width="100%" height="100%" rx="2" ry="2" />
 
       <image width="50" height="50" x="15" y="15" clip-path="url(#iconClip)" href="${iconUrl}" />
 
@@ -142,8 +142,8 @@ export async function makeCompactBadge({
       </g>
 
       <g>
-        <rect width="358" height="25" x="15" y="79" rx="6" ry="6" fill="#00863A" />
-        <text width="358" height="25" x="195" y="95.5" font-weight="500" font-size="12" letter-spacing="-0.5" word-spacing="1.2" text-anchor="middle" fill="#FFFFFF">${slicedButtonMessage}</text>
+        <rect width="358" height="30" x="15" y="79" rx="6" ry="6" fill="#00863A" />
+        <text width="358" height="30" x="195" y="94" text-anchor="middle" dominant-baseline="middle" font-weight="600" font-size="14" letter-spacing="-0.5" word-spacing="1.2" fill="#FFFFFF">${slicedButtonMessage}</text>
       </g>
     </svg>
   `.trim()
